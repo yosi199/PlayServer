e0add246ab4f53d93885ea4438e96821548d3184
yosi199 <yosimizrachi7@gmail.com>
2014-05-06 12:33:57 +0300

    First commit

---
+++ WpfApplication1/FileManagerSingleton.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+
+    public class FileManger
+    {
+        private static FileManger instance;
+        private Thread loadFiles = null;
+        private MainWindow mainW = null;
+        private int fileCount = 0;
+        private int folderCount = 0;
+
+
+        public int filesCount { get { return fileCount; } }
+        public int foldersCount { get { return folderCount; } }
+
+
+        private FileManger() { }
+
+        public static FileManger Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new FileManger();
+                }
+                return instance;
+            }
+        }
+
+        // Pass Ui instance to use the dispatcher
+        public void registerUI(MainWindow main)
+        {
+            this.mainW = main;
+        }
+
+        /// <summary>
+        /// starts a new thread to load files asyncrounsly 
+        /// </summary>
+        /// <param name="di">DirectoryInfo to look for files in</param>
+        public void loadFromDirAsync(DirectoryInfo di)
+        {
+            loadFiles = new Thread(() => FullDirList(di, "*.mp3"));
+            loadFiles.Start();
+            loadFiles.Name = "loadFromDirAsync";
+
+        }
+
+        // Lists for collecting files and folders data
+        List<FileInfo> files = new List<FileInfo>();
+        List<DirectoryInfo> folders = new List<DirectoryInfo>();
+
+        private void FullDirList(DirectoryInfo dir, string searchPattern)
+        {
+
+            try
+            {
+                foreach (FileInfo f in dir.GetFiles(searchPattern))
+                {
+                    files.Add(f);
+                    fileCount++;
+                    //Console.WriteLine("added - " + f.ToString());
+
+                    // Update progressBar Label
+                    mainW.Dispatcher.Invoke(
+                      System.Windows.Threading.DispatcherPriority.Normal,
+                     new Action(
+                delegate()
+                {
+                    mainW.pbLabel.Content = "Indexing - " + f.ToString();
+
+                }
+                 ));
+
+                }
+            }
+
+            catch
+            {
+                Console.WriteLine("Directory {0}  \n could not be accessed!!!!", dir.FullName);
+                return;
+            }
+
+
+            // process each directory
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                folders.Add(d);
+                folderCount++;
+                FullDirList(d, searchPattern);
+            }
+
+ 
+        }
+
+        /// <summary>
+        /// Checks if the async file loading thread still runs
+        /// </summary>
+        /// <returns>bool running or not</returns>
+        public bool isThreadStillLoading()
+        {
+            return (loadFiles.IsAlive);
+        }
+
+
+
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        FileManger fm;
+        bool runCheck = true;
+
+
+
+        public MainWindow()
+        {
+            fm = FileManger.Instance;
+            fm.registerUI(this);
+        }
+
+        private void loadDirBtn(object sender, RoutedEventArgs e)
+        {
+
+            // Create a dialog to choose dir and load songs in a new thread
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            System.Windows.Forms.DialogResult result = dialog.ShowDialog();
+            // Call File Manager Async file loader
+            fm.loadFromDirAsync(new DirectoryInfo(dialog.SelectedPath));
+            // Update UI
+            pb.IsIndeterminate = true;
+            pb.IsEnabled = true;
+            loadBTN.IsEnabled = false;
+
+            // Check if finished loading
+            Thread t = new Thread(() => load());
+            t.Start();
+            t.Name = "LoaderThread";
+        }
+
+        private void load()
+        {
+            while (runCheck)
+            {
+
+                // If Async file loading thread finished...
+                if (!fm.isThreadStillLoading())
+                {
+                    Console.WriteLine("Done running");
+
+                    Dispatcher.Invoke(
+                      System.Windows.Threading.DispatcherPriority.Normal,
+                     new Action(
+                delegate()
+                {
+                    loadBTN.IsEnabled = true;
+                    pb.IsIndeterminate = false;
+                    pb.IsEnabled = false;
+                    runCheck = false;
+                    pbLabel.Content = string.Format("Finished Indexing {0} files in {1} folder - Ready to play.", fm.filesCount, fm.foldersCount);
+
+                }
+                 ));
+
+                }
+            }
+        }
+
+
+    }
+}
